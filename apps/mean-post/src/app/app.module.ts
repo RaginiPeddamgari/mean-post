3ef4ea9fd74eb31169d3d585a7831470c8cfa43d
Ragini Peddamgari <ragini.peddamgari@kaplan.com>
2020-01-22 11:52:09 -0500

    Initial commit

--- apps/mean-post/src/app/app.module.ts
+++ apps/mean-post/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import {MatInputModule} from '@angular/material'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PostCreateComponent],
+  imports: [BrowserModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
