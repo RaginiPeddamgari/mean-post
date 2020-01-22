df688d0134cf807737111630cd4d089d2d9f1754
Ragini Peddamgari <ragini.peddamgari@kaplan.com>
2020-01-22 12:18:21 -0500

    Added material

--- apps/mean-post/src/app/app.module.ts
+++ apps/mean-post/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import {MatInputModule} from '@angular/material'
+import {MatInputModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, MatInputModule],
+  imports: [BrowserModule, MatInputModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
